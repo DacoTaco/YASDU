2df4bf3b32e9290252c8c03df61fe00c5ffac925
DacoTaco <daco_65@hotmail.com>
2018-05-08 19:21:14 +0000

    oh noez! code

---
+++ fileServer.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+import hashlib 
+import subprocess
+import os
+import sys
+import shutil
+import hashlib
+from http.server import BaseHTTPRequestHandler, HTTPServer
+mmcblk1_hash = " "
+mmcblk1boot0_hash = " "
+mmcblk1boot1_hash = " " 
+tsecfw_hash = " "
+tsecfw_size = 3840 
+tsecfw_offset = " "
+
+# HTTPRequestHandler class
+class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
+  global mmcblk1_hash
+  global mmcblk1boot0_hash
+  global mmcblk1boot1_hash
+  global tsecfw_size
+  global tsecfw_hash
+  global tsecfw_offset
+  # GET
+  def do_GET(self):
+        request_path = self.path
+        request_path = request_path.strip('/')
+        download = False
+        size = 0
+        path = None
+        if (
+            request_path == "mmcblk1" or
+            request_path == "mmcblk1boot0" or
+            request_path == "mmcblk1boot1" or
+	        request_path == "tsec_fw"
+        ):
+            download = True
+            if(request_path != "tsec_fw"):
+                path = "/dev/%s"%(request_path)
+                size = subprocess.run(["blockdev","--getsize64",path],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+                size = size.stdout.decode()
+            else:
+                size = tsecfw_size
+        try:
+            if(download):
+                self.send_response(200)
+                self.send_header("Content-Type:","application/octet-stream")
+                self.send_header("Content-Disposition:"," attachment;filename=%s.bin"%request_path)
+                self.send_header("Content-Lenght:","%i"%int(size))
+                self.end_headers()
+                #self.wfile.write(bytes("downloading file...<br>","utf8"))
+                #self.wfile.write(file.read())
+                if(request_path == "tsec_fw"):
+                    tsecfd = open('/dev/mmcblk1boot0','rb')
+                    tsecfd.seek(int(tsecfw_offset,0))
+                    self.wfile.write(tsecfd.read(tsecfw_size))
+                    tsecfd.close()
+                else:
+                    with open(path, 'rb') as content:
+                        shutil.copyfileobj(content, self.wfile)
+            else:
+                fd = open('index.html','r')
+                file_content = fd.read()
+                fd.close()
+
+                #read boot0 hash
+                #output = subprocess.run(["md5sum","/dev/mmcblk1boot0"],stdout=subprocess.PIPE)      
+                #hash = output.stdout.decode('utf-8')
+                #hash = hash[:32]
+                html = file_content.replace("mmcblk1boot0_hash",mmcblk1boot0_hash)
+
+                #read boot1 hash
+                #output = subprocess.run(["md5sum","/dev/mmcblk1boot1"],stdout=subprocess.PIPE)
+                #hash = output.stdout.decode('utf-8')
+                #hash = hash[:32]
+                html = html.replace("mmcblk1boot1_hash",mmcblk1boot1_hash)
+
+                #NAND
+                html = html.replace("mmcblk1_hash",mmcblk1_hash)
+
+                #tsec fw
+                html = html.replace("TSEC_FW_hash",tsecfw_hash)
+
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                self.wfile.write(html.encode())
+        except Exception as e:
+            self.send_error(404,"File Not Found: %s" % self.path)
+            print('error occured on :')
+            print(e) 
+        return
+
+def CheckTSECFW():
+    global tsecfw_hash
+    global tsecfw_offset
+    global tsecfw_size
+    try:
+        file = open('/dev/mmcblk1boot0','rb')
+        #i hate doing this as it makes it load the whole file in memory but...
+        data = file.read()
+        tsecfw_offset = hex(data.index(b'\x4d\x00\x42\xcf'))
+        print('detected tsecfw_offset : %s'%tsecfw_offset)
+        if(tsecfw_offset == None or tsecfw_offset == " "):
+            print('did not find tsec FW!')
+            return
+
+        file.seek(int(tsecfw_offset,0))
+        tsecfw = file.read(tsecfw_size)
+        print('got fw!')
+        m = hashlib.md5()
+        m.update(tsecfw)
+        tsecfw_hash = m.hexdigest()
+        print('hash : %s'%tsecfw_hash)
+        file.close()
+    except Exception as e:
+        print('error occured on :')
+        print(e) 
+    
+    return
+def calc_hashes():
+    global mmcblk1_hash
+    global mmcblk1boot0_hash
+    global mmcblk1boot1_hash
+
+    output = subprocess.run(["md5sum","/dev/mmcblk1"],stdout=subprocess.PIPE)
+    hash = output.stdout.decode('utf-8')
+    mmcblk1_hash = hash[:32]
+    print('mmcblk1 hash : %s'%mmcblk1_hash)
+
+    output = subprocess.run(["md5sum","/dev/mmcblk1boot0"],stdout=subprocess.PIPE)
+    hash = output.stdout.decode('utf-8')
+    mmcblk1boot0_hash = hash[:32]
+    print('boot0 hash : %s'%mmcblk1boot0_hash)
+    
+    output = subprocess.run(["md5sum","/dev/mmcblk1boot1"],stdout=subprocess.PIPE)
+    hash = output.stdout.decode('utf-8')
+    mmcblk1boot1_hash = hash[:32]
+    print('boot1 hash : %s'%mmcblk1boot1_hash)
+
+def run():
+  global mmcblk1_hash
+  global mmcblk1boot0_hash
+  global mmcblk1boot1_hash
+  print('HELLOOOO AND WELCOME!')
+  #check if we are root...
+  if os.geteuid() != 0:
+    exit("You need to have root privileges to run this script.\nDid you use sudo?")
+
+  print('calculating hashes...')
+  print('this could take a while ( +/- 7min on 32GB switch) so get a drink :)')
+  calc_hashes()
+  #print("var : '%s'"%mmcblk1boot1_hash)
+
+  print('retrieving FSEC FW...')
+  CheckTSECFW()
+  
+  print('starting server...')
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('0.0.0.0', 1337 )
+  httpd = HTTPServer(server_address, HTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
+ 
+ 
+run()
